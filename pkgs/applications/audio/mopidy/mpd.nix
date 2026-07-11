9d33a941b3d4a688f113ac9d7e5fbd66c77e892a
xaltsc <hey+dev@xaltsc.dev>
2026-07-11 19:59:29 +0200

    mopidy-mpd: use setuptools_80

--- pkgs/applications/audio/mopidy/mpd.nix
+++ pkgs/applications/audio/mopidy/mpd.nix
@@ -16,7 +16,7 @@ pythonPackages.buildPythonApplication (finalAttrs: {
     hash = "sha256-CeLMRqj9cwBvQrOx7XHVV8MjDjwOosONVlsN2o+vTVM=";
   };
 
-  build-system = [ pythonPackages.setuptools ];
+  build-system = [ pythonPackages.setuptools_80 ];
 
   dependencies = [ mopidy ];
 
